b39f49f7a8e23ec57fdb19012f8bc682adfd7e2a
jameskeywood <jskeywood@gmail.com>
2024-11-09 20:30:06 +0000

    Implement basic command line interface, and link with main.go

--- src/main.go
+++ src/main.go
@@ -5,6 +5,21 @@ import(
 )
 
 func main() {
-    fmt.Println("Hello, World!")
-	fmt.Println(findGitDir(getCurrentDir()))
+	// find command argument
+	command, err := getCommand()
+	if err != nil {
+		panic(err)
+	} else if command == "add" {
+		// find filepaths argument
+		filepaths, err := getFilepaths()
+		if err != nil {
+			panic(err)
+		} else {
+			// add filepaths to links
+			fmt.Println(filepaths)
+		}
+	} else if command == "visual" {
+		// launch visualisation
+		fmt.Println("VISUALISE")
+	}
 }
