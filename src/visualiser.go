55d9dbec9c98397ef3bfec544b3b2bfd9e6ea466
Noah Gitsham <nhgitsham@gmail.com>
2024-11-09 23:01:38 +0000

    WIP: parser

--- src/visualiser.go
+++ src/visualiser.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"strings"
+	"os/exec"
+)
 
 func startServer() {
 	r := gin.Default()
@@ -18,6 +22,33 @@ func startServer() {
 // GIT GRAPH MERMAID CONVERT //
 ///////////////////////////////
 
-func getGitLog() string {
-	return 
+func getGitLog() (string, error) {
+	out, err := exec.Command("git", "log", "--all", "--author-date-order", "--pretty=format:\"%h,%p,%s,%D\"").Output()
+	if (err != nil) {
+		return "", err
+	}
+	return string(out), nil
+}
+
+// Returns hash, parentHash, branchCommit?, mergeCommit?
+func parseLogLine(line string) (hash string, parentHash string, branchCommit string, mergeParentHashes []string) {
+	split := strings.Split(line, ",")
+	hash = split[0]
+
+	parents := strings.Split(split[1], " ")
+	if len(parents) != 1 {
+		parentHash = parents[0]
+	}
+	if len(parents) > 1 {
+		mergeParentHashes = parents[1:]
+	}
+	return
+}
+
+func parseGitLog() string {
+	// Hashmap for commit hash -> branch index
+	// branchOf := make(map[string]int)
+
+	// var parsedOutput string
+	return "hello"
 }
