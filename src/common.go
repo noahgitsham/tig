76bc7381a56507ca589dc0b86fd031321a679436
Noah Gitsham <nhgitsham@gmail.com>
2024-11-09 17:24:36 +0000

    Add function to find git directory

--- src/common.go
+++ src/common.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+    "os"
+	"errors"
+    "path/filepath"
+)
+
+func getCurrentDir() string {
+	currentDirectory, err := os.Getwd()
+	if (err != nil) {
+		panic(err)
+	}
+	return currentDirectory
+}
+
+func dirNotExists(path string) bool {
+	_, err := os.Stat(path)
+	return errors.Is(err, os.ErrNotExist)
+}
+
+// Finds base directory ()
+func findGitDir(path string) string {
+	for (dirNotExists(filepath.Join(path, ".git")) && path != "/") {
+		path = filepath.Dir(path)
+	} 
+	if (path == "/") {
+		return ""
+	}
+	return path
+}
+
 func getHeadCommit() string {
 	return "0"
 }
