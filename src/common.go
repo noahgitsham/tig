17509e11aa88456e7ff9f92919660b3f99745f50
Noah Gitsham <nhgitsham@gmail.com>
2024-11-10 01:35:50 +0000

    Rearrange common functions into files.go

--- src/common.go
+++ src/common.go
@@ -10,29 +10,3 @@ func getHeadCommit() string {
 	return find
 }
 
-func findGitDir(path string) (string, err) {
-	file, err := findBaseDir(path)
-	if (err != nil) {
-		return err
-	}
-	return filepath.Join(file, ".git")
-}
-
-func findTigDir(path string) (string, err) {
-	file, err := findBaseDir(path)
-	if (err != nil) {
-		return err
-	}
-	return filepath.Join(file, ".tig")
-}
-
-func getPublicLinks(path string) (string, err) {
-	file, err := findTigDir(path)
-	if (err != nil) {
-		return err
-	}
-	for _, file := range filepath.Join(file, "links", "public") {
-        fmt.Println(file.Name(), file.IsDir())
-    }
-	return 
-}
--- src/files.go
+++ src/files.go
@@ -13,7 +13,6 @@ func dirNotExists(path string) bool {
 	return errors.Is(err, os.ErrNotExist)
 }
 
-// Finds base directory ()
 func findBaseDir(path string) (string, err) {
 	for (dirNotExists(filepath.Join(path, ".git")) && path != "/") {
 		path = filepath.Dir(path)
@@ -22,4 +21,35 @@ func findBaseDir(path string) (string, err) {
 		return nil, os.ErrNotExist
 	}
 	return path, nil
+}
+
+func findGitDir(path string) (string, err) {
+	file, err := findBaseDir(path)
+	if (err != nil) {
+		return err
+	}
+	return filepath.Join(file, ".git")
+}
+
+func findTigDir(path string) (string, err) {
+	file, err := findBaseDir(path)
+	if (err != nil) {
+		return err
+	}
+	return filepath.Join(file, ".tig")
+}
+
+func getPublicLinkFiles(path string) ([]string, err) {
+	file, err := findTigDir(path)
+	if (err != nil) {
+		return err
+	}
+	files, err := os.ReadDir(filepath.Join(file, "links", "public"))
+	if (err != nil) {
+		return err
+	}
+	for _, file := range files {
+        fmt.Println(file.Name(), file.IsDir())
+    }
+	return 
 }
