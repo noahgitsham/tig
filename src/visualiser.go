1e6f99bfb4cb73277ef67847e059397bc95761c8
sam fahim <sam.fahim@student.manchester.ac.uk>
2024-11-10 10:14:20 +0000

    horizontal graph

--- src/visualiser.go
+++ src/visualiser.go
@@ -14,6 +14,7 @@ import (
 func visualise() {
 	r := gin.Default()
 	r.Static("/static", "./static")
+	r.Static("/images", "./media")
 	r.LoadHTMLGlob("static/*")
 	r.GET("/", func(c *gin.Context) {
 		// c.JSON(200, gin.H{
